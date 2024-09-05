b9b49be4db562d48eff3e2b8d013b5eb02d4166c
Benjamin Cornet <benjamin_cornet@ens.univ-artois.fr>
2024-09-05 09:49:43 +0200

    Initialisation

---
+++ src/main/java/fr/univartois/butinfo/r304/tarotafricain/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Le paquetage {@code fr.univartois.butinfo.r304.tarotafricain} fournit
+ * une implantation du jeu de Tarot Africain.
+ *
+ * @author Votre Nom
+ * @version 0.1.0
+ */
+package fr.univartois.butinfo.r304.tarotafricain;
