93fb09bbdd2c79a776af4b42fa1db32a163727c3
Benjamin Cornet <benjamin_cornet@ens.univ-artois.fr>
2024-09-16 11:15:52 +0200

    :zap: complétion de la méthode play - close #1#2

--- src/main/java/fr/univartois/butinfo/r304/tarotafricain/Trick.java
+++ src/main/java/fr/univartois/butinfo/r304/tarotafricain/Trick.java
@@ -2,6 +2,7 @@ package fr.univartois.butinfo.r304.tarotafricain;
 
 import fr.univartois.butinfo.r304.tarotafricain.cards.Card;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Trick {
@@ -10,22 +11,35 @@ public class Trick {
     private int bestCardIndex;
     private int bestPlayerIndex;
 
+    public Trick(){
+        this.cards = new ArrayList<>();
+        this.bestCardIndex = 0;
+        this.bestPlayerIndex = 0;
+    }
+
+
     public boolean isEmpty(){
         return false;
     }
 
     public Card getBestCard(){
-
+        return cards.get(bestCardIndex);
     }
 
     public int getWinner(){
-
+        return 0;
     }
 
     public void play(int playerIndex, Card card){
-
+        cards.add(card);
+        if(cards.size() == 1){
+            bestCardIndex = 0;
+            bestPlayerIndex = playerIndex;
+        } else{
+            if(card.compareTo(cards.get(bestCardIndex)) > 0){
+                bestCardIndex = cards.size() - 1;
+                bestPlayerIndex = playerIndex;
+            }
+        }
     }
-
-    
-
 }
